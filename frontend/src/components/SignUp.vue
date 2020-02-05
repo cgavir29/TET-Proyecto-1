8cf9e76dc87d6a8df2b3ba203590a8687e60a162
Camilo Gaviria <cgavir29@eafit.edu.co>
2020-02-05 18:26:49 -0500

    SignUp component set up

--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -1,72 +1,36 @@
 <template>
-    <section>
-        <button class="button is-primary"
-            @click="isComponentModalActive = true">
-            Sign up
-        </button>
+  <form action>
+    <div class="modal-card" style="width: auto">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Login</p>
+      </header>
+      <section class="modal-card-body">
+        <b-field label="Email">
+          <b-input type="email" :value="email" placeholder="Your email" required></b-input>
+        </b-field>
 
-        <b-modal :active.sync="isComponentModalActive"
-                 has-modal-card
-                 trap-focus
-                 aria-role="dialog"
-                 aria-modal>
-            <modal-form v-bind="formProps"></modal-form>
-        </b-modal>
-    </section>
+        <b-field label="Password">
+          <b-input
+            type="password"
+            :value="password"
+            password-reveal
+            placeholder="Your password"
+            required
+          ></b-input>
+        </b-field>
+
+        <b-checkbox>Remember me</b-checkbox>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button" type="button" @click="$parent.close()">Close</button>
+        <button class="button is-primary">Login</button>
+      </footer>
+    </div>
+  </form>
 </template>
 
 <script>
-const ModalForm = {
-  props: ['email', 'password'],
-  template: `
-            <form action="">
-                <div class="modal-card" style="width: auto">
-                    <header class="modal-card-head">
-                        <p class="modal-card-title">Login</p>
-                    </header>
-                    <section class="modal-card-body">
-                        <b-field label="Email">
-                            <b-input
-                                type="email"
-                                :value="email"
-                                placeholder="Your email"
-                                required>
-                            </b-input>
-                        </b-field>
-
-                        <b-field label="Password">
-                            <b-input
-                                type="password"
-                                :value="password"
-                                password-reveal
-                                placeholder="Your password"
-                                required>
-                            </b-input>
-                        </b-field>
-
-                        <b-checkbox>Remember me</b-checkbox>
-                    </section>
-                    <footer class="modal-card-foot">
-                        <button class="button" type="button" @click="$parent.close()">Close</button>
-                        <button class="button is-primary">Login</button>
-                    </footer>
-                </div>
-            </form>
-        `
-}
-
 export default {
-  components: {
-    ModalForm
-  },
-  data () {
-    return {
-      isComponentModalActive: false,
-      formProps: {
-        email: 'evan@you.com',
-        password: 'testing'
-      }
-    }
-  }
+  name: 'SignUp'
 }
 </script>
